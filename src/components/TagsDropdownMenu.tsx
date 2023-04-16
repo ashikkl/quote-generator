1feef7f4d866cfa04c452efc7cfbc26fdba08909
Ashik K L <ashikkl19112001@gmail.com>
2023-04-16 15:33:51 +0530

    change the drop menu

--- src/components/TagsDropdownMenu.tsx
+++ src/components/TagsDropdownMenu.tsx
@@ -1,6 +1,11 @@
 import React from "react";
-import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
-import { HamburgerMenuIcon, DotFilledIcon } from "@radix-ui/react-icons";
+import * as Select from "@radix-ui/react-select";
+import classnames from 'classnames';
+import {
+  CheckIcon,
+  ChevronDownIcon,
+  ChevronUpIcon,
+} from "@radix-ui/react-icons";
 import axios from "axios";
 
 const TagsMenu = (props: { onTagSelect: Function }) => {
@@ -15,26 +20,34 @@ const TagsMenu = (props: { onTagSelect: Function }) => {
   function makeMenuItems(item: {
     tagItem: string;
     slug: string;
-    _id: string;
+    key: string;
   }): JSX.Element {
-    return (
-      <DropdownMenu.RadioItem
-        className="text-[13px] leading-none text-bg-200 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-primary-200 data-[highlighted]:text-text-100"
-        value={item.slug}
-        key={item._id}
-      >
-        <DropdownMenu.ItemIndicator className="absolute left-0 w-[25px] inline-flex items-center justify-center">
-          <DotFilledIcon />
-        </DropdownMenu.ItemIndicator>
-        {item.tagItem}
-      </DropdownMenu.RadioItem>
-    );
+    return <SelectItem value={item.slug}>{item.tagItem}</SelectItem>;
   }
+  const SelectItem :any= React.forwardRef(//@ts-ignore
+    ({ children, className, ...props }, forwardedRef) => {
+      return (
+        <Select.Item
+          className={classnames(
+            "text-[13px] leading-none text-bg-100 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-primary-200 data-[highlighted]:text-text-100",
+            className
+          )}
+          {...props}//@ts-ignore
+          ref={forwardedRef}
+        >
+          <Select.ItemText>{children}</Select.ItemText>
+          <Select.ItemIndicator className="absolute left-0 w-[25px] inline-flex items-center justify-center">
+            <CheckIcon />
+          </Select.ItemIndicator>
+        </Select.Item>
+      );
+    }
+  );
   function Menu() {
     return (
       <>
         {tagsList.map((item) =>
-          makeMenuItems({ tagItem: item.name, slug: item.slug, _id: item._id })
+          makeMenuItems({ tagItem: item.name, slug: item.slug, key: item._id })
         )}
       </>
     );
@@ -48,36 +61,35 @@ const TagsMenu = (props: { onTagSelect: Function }) => {
     fetchQuoteTags();
   }, []);
   return (
-    <DropdownMenu.Root>
-      <DropdownMenu.Trigger asChild>
-        <button
-          className="rounded-full w-[35px] h-[35px] inline-flex items-center justify-center text-text-200 bg-primary-300 shadow-[0_2px_10px] shadow-blackA7 outline-none hover:bg-violet3 focus:shadow-[0_0_0_2px] focus:shadow-black"
-          aria-label="Customize options"
-        >
-          <HamburgerMenuIcon />
-        </button>
-      </DropdownMenu.Trigger>
-
-      <DropdownMenu.Portal>
-        <DropdownMenu.Content
-          className="min-w-[220px] bg-primary-300 rounded-md p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
-          sideOffset={5}
-        >
-          <DropdownMenu.Label className="pl-[25px] text-xs leading-[25px] text-text-200">
-            Tags
-          </DropdownMenu.Label>
-          <DropdownMenu.RadioGroup
-            value={selectedTag}
-            onValueChange={(e) => {
-              setSelectedTag(e);
-              handleTagSelect(e);
-            }}
-          ></DropdownMenu.RadioGroup>
-          {Menu()}
-          <DropdownMenu.Arrow className="fill-white" />
-        </DropdownMenu.Content>
-      </DropdownMenu.Portal>
-    </DropdownMenu.Root>
+    <Select.Root>
+      <Select.Trigger
+        className="inline-flex items-center justify-center rounded px-[15px] text-[13px] leading-none h-[35px] gap-[5px] bg-primary-300 hover:text-text-100 text-bg-200 shadow-[0_2px_10px] shadow-black/10 hover:bg-primary-200 focus:shadow-[0_0_0_2px] focus:shadow-black hover:data-[placeholder]:text-text-100 data-[placeholder]:text-bg-200 outline-none"
+        aria-label="Food"
+      >
+        <Select.Value placeholder="Select a Tag" />
+        <Select.Icon className="text-text-200">
+          <ChevronDownIcon />
+        </Select.Icon>
+      </Select.Trigger>
+      <Select.Portal>
+        <Select.Content className="overflow-hidden bg-white rounded-md shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]">
+          <Select.ScrollUpButton className="flex items-center justify-center h-[25px] bg-primary-300 text-text-200 cursor-default">
+            <ChevronUpIcon />
+          </Select.ScrollUpButton>
+          <Select.Viewport className="p-[5px]">
+            <Select.Group>
+              <Select.Label className="px-[25px] text-xs leading-[25px] text-text-200">
+                Tags
+              </Select.Label>
+              {Menu()}
+            </Select.Group>
+          </Select.Viewport>
+          <Select.ScrollDownButton className="flex items-center justify-center h-[25px] bg-primary-300 text-text-200 cursor-default">
+            <ChevronDownIcon />
+          </Select.ScrollDownButton>
+        </Select.Content>
+      </Select.Portal>
+    </Select.Root>
   );
 };
 
