d911da9eeb28bcbcb2a8879a275ff0eb4e7f2fc2
Ashik K L <74865738+ashikkl@users.noreply.github.com>
2023-04-18 09:18:44 +0530

    Update quoteSlice.js

--- src/store/quoteSlice.js
+++ src/store/quoteSlice.js
@@ -3,8 +3,8 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
   quote: {
     _id: "",
-    content: "",
-    author: "",
+    content: "You are not here merely to make a living. You are here in order to enable the world to live more amply, with greater vision, with a finer spirit of hope and achievement. You are here to enrich the world, and you impoverish yourself if you forget the errand.",
+    author: "Woodrow Wilson",
     authorSlug: "",
     length: 0,
     tags: [],
